acaeeae67768718614818a2af7b295eee4066db5
Adriana Silva <adriana.silva@dbccompany.com.br>
2022-07-06 10:54:43 -0300

    Término dos exercícios aula 2

--- temaaula2.js
+++ temaaula2.js
@@ -1,26 +1,56 @@
 /*
-  1) Um funcionário de uma empresa recebe aumento salarial anualmente. Sabe-se que:
-    Esse funcionário foi contratado em 2013, com salário inicial de R$1000,00;
-    A cada ano o funcionário recebe um aumento salarial de 1,5%;
+/*
+  1) Uma revendedora de carros possui 10 carros em sua loja.
+     O estoque da loja está separado por marcas, modelos e anos de fabricação;
+     Da seguinte forma:
+     marcas = [ 'Fiat', 'Ford', 'Honda', 'Volkswagen', 'Land', 'BMW', 'Peugeot', 'Renault' ];
+     modelos = [ 'Uno', 'Mustang', 'Civic', 'Jetta', 'Rover', 'X5', '208', 'Sandero', 'Duster' ];
+     anosFabricacao = [ 2005, 2015, 2018, 2010, 2022, 2022, 2008, 2009, 2007 ];
+     
+    Saiba que todas as informações se encontram na ordem certa, ou seja:
+    A posição zero das marcas condiz com a posição zero dos modelos e o mesmo para o ano de fabricação;
+    Ex: marcas[0] = 'Fiat', modelos[0] = 'Uno' e anosFabricacao[0] = 2005 ou seja nas posições zero temos um Fiat Uno do ano de 2005;
 
-    A partir de 2017 em diante, os aumentos salariais passaram a ser o dobro do percentual do ano anterior
-    Faça um programa que determine e imprima o salário desse funcionário com o passar dos anos até o ano atual,
-    Imprima o percentual de aumento com o passar dos anos
-    E imprima o percentual de aumento total que o salário dele teve em comparação à 2013;
-    
-    OBS: Os salários serão os seguintes, 1000, 1015, 1030.22, 1045.68, 1077.05, 1141.67, 1278.67 e assim por diante...
+    Faça um programa que peça para o usuário informar uma marca e com esse informação o programa imprima no console
+    todas as informações referente ao veículo daquela marca;
+
+    Ex: usuarioInformou = 'Land'
+        console exibiu => 'Land Rover do ano de 2022'
 */
+let  lista_marcas = [ 'Fiat', 'Ford', 'Honda', 'Volkswagen', 'Land', 'BMW', 'Peugeot', 'Renault' ];
+let  lista_modelos = [ 'Uno', 'Mustang', 'Civic', 'Jetta', 'Rover', 'X5', '208', 'Sandero', 'Duster' ];
+let  lista_anos_fabricacao = [ 2005, 2015, 2018, 2010, 2022, 2022, 2008, 2009, 2007 ];
+
+let marca = prompt("Selecione  uma  marca de carro: \n  Fiat , Ford, Honda, Volkswagen, Land, BMW, Peugeot, Renault  ");
+
+let resultadoPosicao = 0
+
+for(let i = 0; i < 8; i++ ){
+    if(marca == lista_marcas[i]){
+        resultadoPosicao = i
+    }
+}
+
+
+console.log(`Usuário informou = "${marca}" \n
+Resultado => "${lista_marcas[resultadoPosicao]}" - "${lista_modelos[resultadoPosicao]}" - "${lista_anos_fabricacao[resultadoPosicao]}"
+`)
+
+
 
 
 /*
 2) Faça um programa que calcule a soma dos primeiros 50 números pares;
 */
- function somaDosPares(x) {
+ function somaDosPares(x = 50) {
     let soma = 0;
     for (let i = 0; i <= x; i += 2) soma += i
     return soma;
 }
-console.log(somaDosPares(50));
+console.log(somaDosPares());
+
+
+
 
 
 /*
@@ -35,20 +65,26 @@ valor = prompt("Digite um valor para tabuada: ");
 n = parseInt(valor);
 
 while(n<=0){
-valor = prompt("O valor precisa ser maior que zero! Digite um valor para tabuada: ");
-n = parseInt(valor);
+    valor = prompt("O valor precisa ser maior que zero! Digite um valor para tabuada: ");
+    n = parseInt(valor);
 }
 
 for(i=0;i<=10;i++){
-
-console.log(i+" x "+n+" = "+i*n+"<Tabuada>");
-
+    console.log(i+" x "+n+" = "+i*n+"<Tabuada>");
 }
     
 /*
 4) Faça um algoritmo que apresente o quadrado de cada um dos números pares entre 1 e 100;
 */
+function passandoNumPares() {
+    for (var i = 0; i < 100; i++) {
+        if ((i % 2) == 0) {
+             console.log('números pares ao quadrado ' + i * i);
+        }
+    }
+}
 
+passandoNumPares();
 
 /*
 5) Faça um algoritmo que imprima no console os valores como se fosse um relógio durante 1 minuto;
@@ -63,6 +99,13 @@ console.log(i+" x "+n+" = "+i*n+"<Tabuada>");
 	59
 	60 (não deve imprimir mais)
 */
+let segundo = 0
+let timer = setInterval(() => {
+    segundo += 1
+    console.log("Segundo - "+segundo)
+    if(segundo == 60)
+        clearInterval(timer);
+}, 1000)
 
 /*
 6) Pergunte ao usuário se ele quer:
@@ -73,33 +116,51 @@ console.log(i+" x "+n+" = "+i*n+"<Tabuada>");
 
 	OBS: façam os devidos tratamentos;
 */
-let selecionarOpcao = 
+let lista_numeros = []
+let opcaoNavegaco = parseInt(prompt("Inserir numero ou finalizar: (0 - Inserir / 1 - Finalizar)"))
+do{
+    let novo_numero = parseInt(prompt("Inserir numero :"))
+    lista_numeros.push(novo_numero)
+    opcaoNavegaco = parseInt(prompt("Inserir numero ou finalizar: (0 - Inserir / 1 - Finalizar)"))
+}while(opcaoNavegaco < 1)
 
-const contratadoEm = 2013;
-const anoAtual = 2022;
-
-const salarioInicial = 1000;
-let salario = salarioInicial;
-let percentualAumento = 1.5 / 100; // Ou 0.015, mesma coisa
-
-console.log(`Ano atual: 2013`);
-console.log(`O salário atual é: R$${salario.toFixed(2)}`);
-
-for(let ano = contratadoEm + 1; ano <= anoAtual; ano++) {
-
-    if(ano >= 2017) {
-        percentualAumento = percentualAumento * 2;
-        // OU percentualAumento *= 2;
-    }
-
-    salario = salario + (salario * percentualAumento);    
-    // OU salario = salario * (percentualAumento + 1)
-    // OU salario *= (percentualAumento + 1)
-
-    console.log(`\n\nAno atual: ${ano}`);
-    console.log(`O percentual de aumento foi: ${percentualAumento * 100}%`);
-    console.log(`O salário atual é: R$${salario.toFixed(2)}`);
+let soma = 0;
+for (let i = 0; i < lista_numeros.length; i++) {
+    soma = soma + lista_numeros[i]
 }
 
-const percentualDeAumentoTotal = ((salario / salarioInicial) * 100);
-console.log(`\n\nEm comparação ao seu salário inicial, você teve um aumento de ${percentualDeAumentoTotal.toFixed(2)}%`);
+alert(`O Resultado da soma de todos os números é: ${soma}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
